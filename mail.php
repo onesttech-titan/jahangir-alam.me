976c31fc85d4d6d5b803d2c96c38bdd89d6c4a37
saiful-onest <web.saifulislam@gmail.com>
2023-09-27 14:27:43 +0600

    mail issue

--- mail.php
+++ mail.php
@@ -1,29 +1,64 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fullname = $_POST["fullname"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    // You can customize these email parameters
-    // $to = "babu.cse46@gmail.com";
-    $to = "saiful.onest@gmail.com";
-    $subject = "New Message from $fullname";
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    // Create an array to store the response data
+    $response = array();
 
-    $message_body = "<html><body>";
-    $message_body .= "<p><b>Full Name:</b> $fullname</p>";
-    $message_body .= "<p><b>Email:</b> $email</p>";
-    $message_body .= "<p><b>Message:</b> $message</p>";
-    $message_body .= "</body></html>";
+    // Configure your email server settings
+    // $mail = new PHPMailer(true);
+    try {
+        //Server settings
 
-    if (mail($to, $subject, $message_body, $headers)) {
-        echo "Message sent successfully!";
-    } else {
-        echo "Message could not be sent. Please try again later.";
+        $phpmailer = new PHPMailer();
+        $phpmailer->isSMTP();
+        $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
+        $phpmailer->SMTPAuth = true;
+        $phpmailer->Port = 2525;
+        $phpmailer->Username = '41fcc99c7d20af';
+        $phpmailer->Password = '061abd42a60e07';
+        $phpmailer->SMTPSecure = false; 
+
+
+        //Recipients
+        $phpmailer->setFrom('test@example.com', 'test');  // Replace with your email and name
+        $phpmailer->addAddress('test@example.com', 'test get');  // Replace with recipient's email and name
+
+        // Content
+        $phpmailer->isHTML(true);
+        $phpmailer->Subject = 'New Message from ';
+        $phpmailer->Body    = "lol";
+        // die(json_encode($mail));
+        try {
+            //code...
+            $phpmailer->send();
+        } catch (\Throwable $th) {
+            die($th);
+        }
+
+        // Email sent successfully
+        $response['success'] = true;
+        $response['message'] = "Message sent successfully!";
+    } catch (Exception $e) {
+        // Email sending failed
+        $response['success'] = false;
+        $response['message'] = "Message could not be sent. Error: {$mail->ErrorInfo}";
     }
+
+    // Return the JSON response
+    header('Content-type: application/json');
+    echo json_encode($response);
 } else {
     echo "Invalid request method. Please submit the form.";
 }
